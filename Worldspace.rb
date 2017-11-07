764828287f9afc8734348c468ef0467c0f8453d0
Tim <Timwilliamson1337@gmail.com>
2017-11-07 17:10:29 +0000

    Bugfix

--- Worldspace.rb
+++ Worldspace.rb
@@ -29,13 +29,13 @@ class WorldSpace
   def isometric(x,y) #xy co-ordinates to isometric
     newx = @origin.x + x*tilesize.x + y*tilesize.x
     newy = @origin.y + x*tilesize.y - y*tilesize.y
-    xy.new(newx,newy)
+    XY.new(newx,newy)
   end
 
   def draw #refactor into asset and tile classes? then isometric would have to be as well, or in a seperate space?
     @map.each.with_index {|row, yi| row.each.with_index {|tile, xi|
       xy = isometric((xi),(yi))
-      tile.image.draw(xy[0],xy[1],0)
+      tile.image.draw(xy.x,xy.y,0)
       }}
     @players.each {|key,player| player.assets.each {|asset|
       asset.model.draw(isometric(asset.position.x, asset.position.y))
